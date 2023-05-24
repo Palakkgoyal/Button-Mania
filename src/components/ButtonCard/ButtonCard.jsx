f51fff6993a52c0853f8b34e52bde41597836d12
Palak <Palakgoyal0304@gmail.com>
2023-05-24 15:07:20 +0530

    Create basic structure of app and Initial commit

---
+++ src/components/ButtonCard/ButtonCard.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import './ButtonCard.css'
+
+const ButtonCard = ({Children}) => {
+  return (
+    <div className='card'>
+        hello
+      {Children}
+    </div>
+  )
+}
+
+export default ButtonCard
---
+++ src/components/Buttons/AllButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ButtonCard from '../ButtonCard/ButtonCard'
+import './AllButton.css'
+
+const AllButton = () => {
+  return (
+    <main className='btn-container'>
+      <ButtonCard>
+        
+      </ButtonCard>
+      <ButtonCard>
+
+      </ButtonCard>
+      <ButtonCard>
+
+      </ButtonCard>
+      <ButtonCard>
+
+      </ButtonCard>
+    </main>
+  )
+}
+
+export default AllButton
