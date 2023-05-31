c4b38b5ea935f8d27b7f684f9adae45e696946d6
Palak <Palakgoyal0304@gmail.com>
2023-05-31 18:39:42 +0530

    Remove unnecessary code

--- src/components/Buttons/AllButton.jsx
+++ src/components/Buttons/AllButton.jsx
@@ -14,12 +14,6 @@ const AllButton = () => {
         authorName="Palak Goyal"
         githubLink="https://github.com/palakkgoyal"
       />
-
-      <ButtonCard />
-
-      <ButtonCard />
-
-      <ButtonCard />
     </main>
   )
 }
